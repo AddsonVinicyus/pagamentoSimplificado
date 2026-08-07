7226ea5f2514c9385f6576cdc4f5ec005dd5a58f
AddsonVinicyus <addsonvinicyus@gmail.com>
2026-08-07 11:41:33 -0300

    create Transaction entity

---
+++ src/main/java/com/adx/pagamentoSimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.adx.pagamentoSimplificado.domain.transaction;
+
+import com.adx.pagamentoSimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    private User payer;
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    private User payee;
+    private LocalDateTime timestamp;
+
+}
